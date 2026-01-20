e28ec87f30e8e3133d83a6d779174da18ac69389
Yash Jain <100947501+yash343328@users.noreply.github.com>
2026-01-20 09:38:04 +0530

    Create SwapWithoutThirdVariable.java

---
+++ SwapWithoutThirdVariable.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class SwapWithoutThirdVariable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
