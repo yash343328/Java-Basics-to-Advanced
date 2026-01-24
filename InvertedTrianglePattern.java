299c93a17073230c3aba9f03057064d072b9b6c0
Yash Jain <100947501+yash343328@users.noreply.github.com>
2026-01-24 14:51:35 +0530

    Create InvertedTrianglePattern.java

---
+++ InvertedTrianglePattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class InvertedTrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
